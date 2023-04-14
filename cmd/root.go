c78f2bff1adb8f73583d49c49acec37b4165ddff
Mark Cornick <mcornick@mcornick.com>
2023-04-13 23:04:13 -0400

    fix: ignore error on deferred Body.Close
    Signed-off-by: Mark Cornick <mcornick@mcornick.com>

--- cmd/root.go
+++ cmd/root.go
@@ -109,7 +109,9 @@ func callAPI(method string, path string, bodyReader io.Reader, auth bool) ([]byt
 	if err != nil {
 		return nil, err
 	}
-	defer response.Body.Close()
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(response.Body)
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		return nil, err
