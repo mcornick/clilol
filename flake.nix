b01cb946ab1e1d74d8518365283346167f0c79fd
Mark Cornick <mcornick@mcornick.com>
2025-06-09 19:10:39 -0400

    chore: simpler nix flake setup
    Signed-off-by: Mark Cornick <mcornick@mcornick.com>

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = with pkgs; [
+              go
+              go-task
+              go-tools
+              gofumpt
+              golangci-lint
+              gopls
+              universal-ctags
+            ];
+          };
+      }
+    );
+}
